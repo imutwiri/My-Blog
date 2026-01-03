1c5926e4ee2ca4f1d28512020470f717a6ff11f1
kinoti <kinoti@example.com>
2026-01-03 16:58:47 +0300

    Add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import appconfig from "./config/app.config";
 
 import allRoutesv1 from "./routes/v1.route";
@@ -15,5 +15,13 @@ app.use("/health", (req: Request, res: Response) => {
 });
 
 app.use("/api", allRoutesv1);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.log('This is an error handler');
+
+    res.json({
+        error: err,
+    });
+});
 
 export default app;
