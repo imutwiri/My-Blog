16c6d15f2a23ca2bad97c523caa95766976c48aa
kinoti <kinoti@example.com>
2026-01-04 01:44:41 +0300

    Add database config

--- src/modules/users/routes/user.route.ts
+++ src/modules/users/routes/user.route.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller";
+import { UserService } from "../services/user.service";
+import pool from "../../../config/database.config";
 
 const router = Router();
 
-const userController = new UserController();
+const userService = new UserService(pool);
+const userController = new UserController(userService);
 
-router.get("/me", userController.getProfile);
+router.get("/me", userController.getProfile.bind(userController));
 
-router.get("/:userID", userController.getUser);
+router.get("/:userID", userController.getUser.bind(userController));
 
 
 export default router;
