88175ed57d73f16a00373948804dd1d758e74c4f
kinoti <kinoti@example.com>
2026-01-04 01:47:25 +0300

    Add controllers

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,13 +1,16 @@
 import { Request, Response, NextFunction, Router } from "express";
+import { UserService } from "../services/user.service";
 
 export class UserController {
     public router = Router();
 
-    constructor() {}
+    constructor(private userService: UserService) {}
 
     async getUser(req: Request, res: Response, next: NextFunction) {
         try {
-            res.status(200).json({ message: "Get User A" });
+            const { userID } = req.params;
+            const user = await this.userService.getUserByID(Number(userID));
+            res.status(200).json({ data: user });
         }catch (error) {
             console.log(error);
             next(error);
